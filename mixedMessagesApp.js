ee845bcb5f6a1e51cc8ffbc76447514f0edf5e96
ayohizato <ayoshizato.ca@gmail.com>
2021-11-23 16:23:13 -0800

    Loop function included

--- mixedMessagesApp.js
+++ mixedMessagesApp.js
@@ -5,9 +5,10 @@
 const strArray1 = ['Your day is going to be', 'Your future seems to be', 'The life looks', 'Your energy seems'];
 const strArray2 = ['amazing', 'wonderful', 'brilliant', 'great', 'fantastic'];
 const strArray3 = ['promissing', 'fortunate', 'full of surprises', 'full of friends'];
-const strArray4 = ['Smiling', 'Laughing', 'Socilalizing', 'Making connections', 'Playing', 'Engaging in causes you believe' ]
-const strArray5 = ['will make you', 'will turn your day', 'will make your life even' ]
-const strArray6 = ['happier', 'shiner', 'brighter', 'warmer', 'stronger']
+const strArray4 = ['Smiling', 'Laughing', 'Socilalizing', 'Making connections', 'Playing', 'Engaging in causes you believe' ];
+const strArray5 = ['will make you', 'will turn your day', 'will make your life even' ];
+const strArray6 = ['happier', 'shiner', 'brighter', 'warmer', 'stronger'];
+
 
 // Function that generates random phrases based on the content of the declared arrays
 let newPhrase = (strInput1, strInput2, strInput3, strInput4, strInput5, strInput6) => {
@@ -19,8 +20,20 @@ let newPhrase = (strInput1, strInput2, strInput3, strInput4, strInput5, strInput
     const strTemp6 = strInput6[Math.floor(Math.random() * (strInput6.length))];
    
     let phraseTemp = `${strTemp1} ${strTemp2} and ${strTemp3}! ${strTemp4} ${strTemp5} ${strTemp6}!`;
+
     return phraseTemp
 }
 
-// Calling the function that generates a new phrase
-console.log(newPhrase(strArray1, strArray2, strArray3, strArray4, strArray5, strArray6))
+
+// Function that creates N phrases according to the quantity requested by the user
+let phrasesArray = [];
+let phrasesGenerator = (quantity) => {
+    for (i=0; i<quantity; i++) {
+        phrasesArray.push(newPhrase(strArray1, strArray2, strArray3, strArray4, strArray5, strArray6));
+}
+    return phrasesArray
+    }
+
+
+// Calling the function with the number of phrases to be generated    
+console.log(phrasesGenerator(20));
